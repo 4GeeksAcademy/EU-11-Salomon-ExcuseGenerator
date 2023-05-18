344dca7c6fb299f7b3520ab2ed0be4fe23813d1f
SalomonFranco <110070841+SalomonFranco@users.noreply.github.com>
2023-05-18 20:58:07 +0000

    excuseGeneratorUpdated

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let somebody = ["A monkey", "My girlfriend", "A racoon", "My brother"];
+  let action = ["distroyed", "stole", "broke", "snatched"];
+  let whatever = ["my homework", "the keys", "my motorcycle", "my car"];
+  let when = [
+    "before the interview",
+    "when I was about to go out",
+    "during my breakfast",
+    "while I was playing videogames"
+  ];
+
+  let somebodyExcuse = Math.floor(Math.random() * somebody.length);
+  let actionExcuse = Math.floor(Math.random() * action.length);
+  let whateverExcuse = Math.floor(Math.random() * whatever.length);
+  let whenExcuse = Math.floor(Math.random() * when.length);
+
+  let excuse =
+    somebody[somebodyExcuse] +
+    " " +
+    action[actionExcuse] +
+    " " +
+    whatever[whateverExcuse] +
+    " " +
+    when[whenExcuse];
+  document.getElementById("stringExcuse").textContent = excuse;
 };
